798a100c810e9fbb9b4297db27bbbbd874b4733b
SandeepPeetla <sandeeppeetla@gmail.com>
2025-03-13 17:26:29 +0530

    Refactor CSS for consistency and improve JavaScript data fetching logic

--- ecom.js
+++ ecom.js
@@ -1,25 +1,18 @@
 async function getData() {
-    const data = await fetch("https://fakestoreapi.com/products");
-    const jsondata =await data.json();
-    console.log(jsondata);
+  const data = await fetch("https://fakestoreapi.com/products");
+  const jsondata = await data.json();
+  console.log(jsondata);
 
-    jsondata.forEach((element) => {
-        const prodcard = document.createElement('div');
-        prodcard.classList.add("product-card");
+  jsondata.forEach((element) => {
+    const prodcard = document.createElement("div");
+    prodcard.classList.add("product-card");
 
-
-
-    prodcard.innerHTML = 
-    `<img src="${element.image}" alt="">
+    prodcard.innerHTML = `<img src="${element.image}" alt="">
     <h2 "product-title">${element.title}</h2>
     <p class="product-discription">${element.description}</p>
     <p class="product-price">${element.price}</p>`;
 
     document.getElementById("products").appendChild(prodcard);
-
-
-}
-  );
-
+  });
 }
 getData();
